f65b9d3b8265a2ce4ceeeab110ca580aada17ef0
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 20:58:02 +0530

    adding FTPFNode class

--- index.ts
+++ index.ts
@@ -174,3 +174,45 @@ class Animator {
         }
     }
 }
+
+class FTPFNode {
+    
+    next : FTPFNode 
+    prev : FTPFNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new FTPFNode(0)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawFTPFNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : FTPFNode {
+        var curr : FTPFNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
