ad307047be5c45589f8549fa687f726c4fe63ff0
Anwesh43 <anweshthecool0@gmail.com>
2020-11-01 20:43:07 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -23,8 +23,79 @@ class ScaleUtil {
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
-    
+
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawTriPathFill(context : CanvasRenderingContext2D, size : number, sf5 : number) {
+        context.save()
+        context.beginPath()
+        context.moveTo(0, -size)
+        context.lineTo(w / 2 - size / 2, -size)
+        context.lineTo(w / 2, -h)
+        context.lineTo(w / 2 + size / 2, -size)
+        context.lineTo(w, -size)
+        context.lineTo(w, 0)
+        context.lineTo(0, 0)
+        context.lineTo(0, -size)
+        context.clip()
+        context.fillRect(0, -h, w * sf5, h)
+        context.restore()
+    }
+
+    static drawFullscreenTriFill(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(0, h)
+        DrawingUtil.drawLine(
+            context,
+            0,
+            -size,
+            (w - size / 2) * ScaleUtil.divideScale(sf, 0, parts),
+            -size
+        )
+        DrawingUtil.drawLine(
+            context, 
+            w - size / 2,
+            -size, 
+            w - size / 2 + size * 0.5 * ScaleUtil.divideScale(sf, 1, parts),
+            -size - (h - size) * ScaleUtil.divideScale(sf, 1, parts) 
+        )
+        DrawingUtil.drawLine(
+            context, 
+            w / 2, 
+            -h, 
+            w / 2 + size * 0.5 * ScaleUtil.divideScale(sf, 2, parts),
+            -h + (h - size) * ScaleUtil.divideScale(sf, 2, parts)
+        )        
+        DrawingUtil.drawLine(
+            context, 
+            w / 2 + size / 2,
+            -size, 
+            w / 2 + size / 2 + (w / 2 - size / 2) * ScaleUtil.divideScale(sf, 3, parts),
+            -size
+        )
+        DrawingUtil.drawTriPathFill(context, size, ScaleUtil.divideScale(sf, 4, parts))
+        context.restore()
+    }
+
+    static drawFTPFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawFullscreenTriFill(context, scale)
     }
 }
